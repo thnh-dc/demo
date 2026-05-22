fad021fc6eb16987031290a6ec44c0d508e67284
thnh-dc <duoc4751050391@st.qnu.edu.vn>
2026-05-22 22:00:56 +0700

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -10,10 +10,10 @@
     <title>FD Tech</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="/FD-Tech/assets/css/style_chung.css">
-    <link rel="stylesheet" href="/FD-Tech/assets/css/header.css">
-    <link rel="stylesheet" href="/FD-Tech/assets/css/footer.css">
-    <link rel="stylesheet" href="/FD-Tech/assets/css/style_ai_assistant.css">
+    <link rel="stylesheet" href="/assets/css/style_chung.css">
+    <link rel="stylesheet" href="/assets/css/header.css">
+    <link rel="stylesheet" href="/assets/css/footer.css">
+    <link rel="stylesheet" href="/assets/css/style_ai_assistant.css">
     <?php
     if (isset($custom_css)) {
         echo $custom_css;
